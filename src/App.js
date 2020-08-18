bb76ee1e923d517f3bfe0dcd627d9068e8cc979c
Iyemay Gonzalez Vargas <iyemay96@gmail.com>
2020-08-18 17:24:30 -0500

    Including the HeaderCatalogue component in the App component and its style in the index.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import HeaderCatalogue from "./components/Header/HeaderCatalogue";
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+        <HeaderCatalogue />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import HeaderCatalogue from "./components/Header/HeaderCatalogue.scss";
 
 
 
