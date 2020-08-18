1dea72df10e1982cfb0408ede525b6ac6e8bf67e
Iyemay Gonzalez Vargas <iyemay96@gmail.com>
2020-08-18 17:22:45 -0500

    Created the Header Catalogue component and its style file

---
+++ src/components/Header/HeaderCatalogue.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HeaderCatalogue() {
+    return(
+        <div className="d-flex justify-content-center py-4">
+            <p className="header-pokemon">This is the Header Component</p>
+        </div>
+    );
+}
+
+export default HeaderCatalogue;
