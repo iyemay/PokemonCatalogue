8004326639269e555f09b5ae5b561b8aeec03ceb
Iyemay Gonzalez Vargas <iyemay96@gmail.com>
2020-08-18 17:14:09 -0500

    Sass included in the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+
     </div>
   );
 }
