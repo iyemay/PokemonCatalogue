ed04d1bbc731395be7db3c0c8657d0c6debdc913
Iyemay Gonzalez Vargas <iyemay96@gmail.com>
2020-08-19 13:47:43 -0500

    Included thunk, logger dependencies, Provider component and the store

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger'
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import HeaderCatalogue from "./components/HeaderCatalogue/HeaderCatalogue.scss";
 import DropdownPokemonRender from "./components/DropdownPokemonRender/DropdownPokemonRender.scss";
 import CardPokemon from "./components/CardPokemon/CardPokemon.scss";
-import ListCardPokemon from "./components/ListCardPokemon/ListCardPokemon.scss"
+import ListCardPokemon from "./components/ListCardPokemon/ListCardPokemon.scss";
+import { getPokemons } from "./actions/actions";
+import pokemonsReducer from "./reducers/pokemonsReducer";
 
+const store = createStore(pokemonsReducer, applyMiddleware(thunk, logger));
+
+store.dispatch(getPokemons());
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
