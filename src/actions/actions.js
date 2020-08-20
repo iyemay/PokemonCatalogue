a34dd9d5b17f1d8ed5b8914c4bd513963e89ba62
Iyemay Gonzalez Vargas <iyemay96@gmail.com>
2020-08-20 14:44:23 -0500

    Created getPokemonAttacks action

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,8 +1,36 @@
 import axios from 'axios';
 
 export const GET_POKEMONS = 'GET_POKEMONS';
-export const GET_POKEMON_DETAILS = 'GET_POKEMON_DETAILS';
+export const POKEMON_ATTACKS_LOADING = 'POKEMON_ATTACKS_LOADING';
+export const POKEMON_ATTACKS_RECEIVED = 'POKEMON_ATTACKS_RECEIVED';
+export const POKEMON_ATTACKS_FAILED = 'POKEMON_ATTACKS_FAILED';
 const apiUrl = 'https://pokeapi.co/api/v2';
+
+export function getPokemonsAction(pokemons) {
+    return {
+        type: GET_POKEMONS,
+        pokemons: pokemons
+    }
+}
+
+export function pokemonAttacksLoading() {
+    return {
+        type: POKEMON_ATTACKS_LOADING
+    }
+}
+
+export function pokemonAttacksReceived(pokemonAttacks) {
+    return {
+        type: POKEMON_ATTACKS_RECEIVED,
+        pokemonAttacks: pokemonAttacks
+    }
+}
+
+export function pokemonAttacksFailed() {
+    return {
+        type: POKEMON_ATTACKS_FAILED
+    }
+}
 
 export const getPokemons = () => {
     return (dispatch) => {
@@ -18,10 +46,7 @@ export const getPokemons = () => {
                 });
 
                 Promise.all(pokemonRequests).then((pokemons) => {
-                    dispatch({
-                        type: GET_POKEMONS,
-                        pokemons: pokemons
-                    });
+                    dispatch(getPokemonsAction(pokemons));
                 });
             })
             .catch(error => {
@@ -31,13 +56,38 @@ export const getPokemons = () => {
     };
 };
 
+export const getPokemonAttacks = (pokemon) => {
+    return (dispatch) => {
+        dispatch(pokemonAttacksLoading());
+
+        return pokemonMovesPromise(pokemon)
+            .then((moves) => {
+                let pokemonAttacks = []
+                    // Meter los ataques recibidos dentro del arreglo
+                moves.forEach((obj) => {
+                    pokemonAttacks = [
+                        ...pokemonAttacks,
+                        obj.move
+                    ]
+                })
+
+                dispatch(pokemonAttacksReceived(pokemonAttacks));})
+            .catch(error => {
+                console.log(error);
+                dispatch(pokemonAttacksFailed());
+                throw(error);
+            });
+    }
+}
+
 const pokemonPromise = (pokemon, id) => {
     const imgPromise = pokemonImagePromise(pokemon);
     return imgPromise.then((image) => {
         return {
             id: id,
             name: pokemon.name,
-            image: image
+            image: image,
+            details: pokemon.url
         };
     });
 }
@@ -46,6 +96,17 @@ const pokemonImagePromise = (pokemon) => {
     return axios.get(pokemon.url)
         .then(response => {
             return response.data.sprites.front_default;
+        })
+        .catch(error => {
+            console.log(error);
+            throw(error);
+        });
+}
+
+const pokemonMovesPromise = (pokemon) => {
+    return axios.get(pokemon.details)
+        .then(response => {
+            return response.data.moves;
         })
         .catch(error => {
             console.log(error);
--- src/reducers/pokemonsReducer.js
+++ src/reducers/pokemonsReducer.js
@@ -1,12 +1,38 @@
-import { GET_POKEMONS } from "../actions/actions";
+import {POKEMON_ATTACKS_LOADING, POKEMON_ATTACKS_RECEIVED, POKEMON_ATTACKS_FAILED, GET_POKEMONS} from "../actions/actions";
+export const PENDING = "PENDING";
+export const FULLFILLED = "FULLFILLED";
+export const FAILED = "FAILED";
 
-const initialState = { pokemonsList: [] }
+const initialState = {
+    pokemonsList: [],
+    pokemonAttacksRetrievalStatus: PENDING, // ["PENDING", "FULLFIELD", "FAILED"]
+    currentPokemonAttacks: []
+}
 
-export default function pokemonsReducer(state = initialState, action) {
+export default function pokemonsReducer(state
+                                            = initialState, action) {
     switch (action.type) {
         case GET_POKEMONS:
             return {
+                ...state,
                 pokemonsList: action.pokemons
+            }
+        case POKEMON_ATTACKS_LOADING:
+            return {
+                ...state,
+                pokemonAttacksRetrievalStatus: PENDING,
+                currentPokemonAttacks: []
+            }
+        case POKEMON_ATTACKS_RECEIVED:
+            return {
+                ...state,
+                pokemonAttacksRetrievalStatus: FULLFILLED,
+                currentPokemonAttacks: action.pokemonAttacks
+            }
+        case POKEMON_ATTACKS_FAILED:
+            return {
+                ...state,
+                pokemonAttacksRetrievalStatus: FAILED
             }
         default:
             return state;
