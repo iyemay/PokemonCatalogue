920d51d1bbb624475868705e83dc5601e532f0d5
Iyemay Gonzalez Vargas <iyemay96@gmail.com>
2020-08-21 01:14:10 -0500

    Including background and header styles

--- src/components/HeaderCatalogue/HeaderCatalogue.js
+++ src/components/HeaderCatalogue/HeaderCatalogue.js
@@ -3,7 +3,7 @@ import React from "react";
 function HeaderCatalogue() {
     return(
         <div className="d-flex justify-content-center py-4">
-            <p className="header-pokemon">Pokémon Catalogue</p>
+            <strong><p className="header-pokemon">Pokémon Catalogue</p></strong>
         </div>
     );
 }
